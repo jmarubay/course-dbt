df82aa5ffd6ff157b7b40bc27b89dbd68467197a
Julia Marubayashi <jmarubay@greenpeace.org>
2023-01-11 22:05:24 +0000

    My first dbt model?

---
+++ greenery/models/staging/postgres/stg_postgres.sql
@@ -0,0 +1,21 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+SELECT
+     order_id AS order_guid,
+     user_id AS user_guid,
+     promo_id AS promo_desc,
+     address_id AS address_guid,
+     created_at::timestampntz AS created_at_utc,
+     order_cost,
+     shipping_cost,
+     order_total,
+     tracking_id AS tracking_guid,
+     shipping_service,
+     estimated_delivery_at::timestampntz AS estimated_delivery_at_utc,
+     delivered_at::timestampntz AS delivery_at_utc,
+     status
+ FROM {{ source('postgres', 'orders') }}
