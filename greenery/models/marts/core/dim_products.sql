1ead2a9805edba662d636cdafb85068d3a2f6415
Julia Marubayashi <jmarubay@greenpeace.org>
2023-01-23 01:23:22 +0000

    Update model dim_products

--- greenery/models/marts/core/dim_products.sql
+++ greenery/models/marts/core/dim_products.sql
@@ -1,17 +1,7 @@
 SELECT
-    u.user_guid,
-    u.email,
-    u.first_name,
-    u.last_name,
-    u.phone_number,
-    u.created_at_utc AS user_created_at,
-    u.updated_at_utc AS user_updated_at,
-    a.address_guid,
-    a.address,
-    a.zipcode,
-    a.state,
-    a.country
+    product_guid,
+    name AS product_name,
+    price,
+    inventory
 
-FROM {{ ref('stg_postgres__users') }} AS u
-JOIN {{ ref('stg_postgres__addresses') }} AS a
-USING(address_guid)
+FROM {{ ref('stg_postgres__products') }} 
