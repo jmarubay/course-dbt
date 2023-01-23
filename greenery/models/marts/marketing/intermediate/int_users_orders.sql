e1b296db95ad06d347823406a13a9e259e14dfc3
Julia Marubayashi <jmarubay@greenpeace.org>
2023-01-23 01:44:31 +0000

    Created an intermediate model int_users_orders

---
+++ greenery/models/marts/marketing/intermediate/int_users_orders.sql
@@ -0,0 +1,15 @@
+SELECT
+    u.user_guid,
+    u.email,
+    u.user_created_at,
+    u.user_updated_at, 
+    u.address_guid,
+    o.order_guid,
+    o.order_created_at,
+    o.order_delivered_at,
+    o.order_cost, 
+    o.order_status
+
+FROM {{ ref('dim_users') }} AS u
+JOIN {{ ref('fct_orders') }} AS o
+USING(user_guid)
