42e68670989ede7eb595d4764e6f521d3092ae4e
Julia Marubayashi <jmarubay@greenpeace.org>
2023-01-23 00:42:50 +0000

    Created marts folders and my first intermediate model

---
+++ greenery/models/marts/core/dim_users.sql
@@ -0,0 +1,23 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+SELECT
+    u.user_guid,
+    u.email,
+    u.first_name,
+    u.last_name,
+    u.phone_number,
+    u.created_at_utc AS user_created_at,
+    u.updated_at_utc AS user_updated_at,
+    a.address_guid,
+    a.address,
+    a.zipcode,
+    a.state,
+    a.country
+
+FROM {{ ref('stg_postgres__users') }} AS u
+JOIN {{ ref('stg_postgres__addresses') }} AS a
+USING(address_guid)
