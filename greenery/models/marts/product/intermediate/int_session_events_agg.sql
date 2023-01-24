3a04459ecf61bb3cb83a79223c98f84ea428998e
Julia Marubayashi <jmarubay@greenpeace.org>
2023-01-24 01:28:18 +0000

    Fix typo in int_session_events_agg.sql

--- greenery/models/marts/product/intermediate/int_session_events_agg.sql
+++ greenery/models/marts/product/intermediate/int_session_events_agg.sql
@@ -9,7 +9,7 @@ SELECT
     SUM(CASE WHEN event_type = 'add_to_cart' then 1 else 0 end) AS add_to_cart, 
     SUM(CASE WHEN event_type = 'checkout' then 1 else 0 end) AS checkout, 
     SUM(CASE WHEN event_type = 'package_shipped' then 1 else 0 end) AS packages_shipped, 
-    SUM(CASE WHEN event_type = 'page_views' then 1 else 0 end) AS page_view, 
+    SUM(CASE WHEN event_type = 'page_view' then 1 else 0 end) AS page_view, 
     MIN(event_created_at) AS first_session_event_at_utc,
     MAX(event_created_at) AS last_session_event_at_utc 
     
