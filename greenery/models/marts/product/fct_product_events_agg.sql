ae2e1a1a95f3a292b1ef42fc90507e7708d8798c
Julia Marubayashi <jmarubay@greenpeace.org>
2023-01-29 22:03:00 +0000

    Update fct_product_events_agg

--- greenery/models/marts/product/fct_product_events_agg.sql
+++ greenery/models/marts/product/fct_product_events_agg.sql
@@ -14,12 +14,6 @@ WITH final AS(
     {% for event_type in event_types %}
     , SUM(CASE WHEN event_type = '{{ event_type }}' then 1 else 0 end) as {{ event_type }}s
     {% endfor %}
-    --SUM(CASE WHEN e.event_type = 'add_to_cart' then 1 else 0 end) AS add_to_carts, 
-    --SUM(CASE WHEN e.event_type = 'checkout' then 1 else 0 end) AS checkouts, 
-    --SUM(CASE WHEN e.event_type = 'package_shipped' then 1 else 0 end) AS packages_shippeds, 
-    --SUM(CASE WHEN e.event_type = 'page_view' then 1 else 0 end) AS page_views, 
-    , MIN(event_created_at) AS first_session_event_at
-    , MAX(event_created_at) AS last_session_event_at 
     
 FROM {{ ref('int_events') }} AS e 
 LEFT JOIN {{ ref('int_product_events_checkout') }} AS p
